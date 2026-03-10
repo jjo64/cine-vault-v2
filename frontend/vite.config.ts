1b41a2c4a2f5964cf54eef4108123e8c0e1ae61e
Nat <nat111vill@gmail.com>
2026-03-10 01:02:22 +0100

    cambiamos el puerto

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5000,
+    strictPort: true, // Para que falle si el puerto 5000 está ocupado en lugar de usar otro
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4000/',
+        changeOrigin: true,
+      },
+    },
+  }
 })
